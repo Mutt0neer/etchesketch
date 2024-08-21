5410c2dc60a0fe97dc4b71965ed72d049548233d
Mutt0neer <milluzzi26@students.hopkins.edu>
2024-08-20 23:54:09 -0400

    stuff

--- javascript.js
+++ javascript.js
@@ -1,17 +1,30 @@
 const canvas = document.getElementById("canvas");
-for(let i=0;i<100;i++){
-    let row = document.createElement("div");
-    row.classList.add("rows");
-    canvas.appendChild(row);
-    for(let o=0;o<100;o++){
-        let box = document.createElement("div");
-        box.classList.add("boxes");
-        row.appendChild(box);
-        box.addEventListener("mousemove", () =>{
+const button = document.getElementById("button");
+let size = 100;
+button.addEventListener("click", () =>{
+    size = Number(prompt("what size?"));
+    makecanvas();
+})
+makecanvas();
+function makecanvas(){
+    canvas.innerHTML="";
+    for(let i=0;i<size;i++){
+        let row = document.createElement("div");
+        row.classList.add("rows");
+        canvas.appendChild(row);
+        for(let o=0;o<size;o++){
+            let box = document.createElement("div");
+            let color=0;
+            box.classList.add("boxes");
             box.style.backgroundColor="black";
-        })   
-        document.body.addEventListener("mouse", () =>{
-            
-        })
+            box.style.opacity=0;
+            row.appendChild(box);
+
+            box.addEventListener("mouseover", () =>{
+                color=color+.1;
+                box.style.opacity=color;
+            })   
+        }
     }
 }
+
